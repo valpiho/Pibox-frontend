d3bdf3c1a9bb7e8432be04c9e163fea9c696dc09
Valentin Piho <val.piho@gmail.com>
2021-02-03 19:06:50 +0200

    Ideas for future

--- src/app/views/layout/sidebar/menu.ts
+++ src/app/views/layout/sidebar/menu.ts
@@ -2,8 +2,107 @@ import { MenuItem } from './menu.model';
 
 export const MENU: MenuItem[] = [
   {
+    label: 'Main',
+    isTitle: true
+  },
+  {
     label: 'Profile',
     icon: 'home',
     link: '/general/profile'
   },
+  {
+    label: 'Email',
+    icon: 'mail',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Chat',
+    icon: 'message-square',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Calendar',
+    icon: 'calendar',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Documents',
+    icon: 'file',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Payments',
+    icon: 'credit-card',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Groups',
+    isTitle: true
+  },
+  {
+    label: 'Find group',
+    icon: 'search',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'Create group',
+    icon: 'plus',
+    link: '',
+    badge: {
+      variant: 'primary',
+      text: 'Soon',
+    }
+  },
+  {
+    label: 'My groups',
+    isTitle: true
+  },
+  {
+    label: 'Pibox',
+    link: '',
+    badge: {
+      variant: 'info',
+      text: 'Owner',
+    }
+  },
+  {
+    label: 'SK Eola',
+    link: '',
+    badge: {
+      variant: 'info',
+      text: 'Guest',
+    }
+  },
+  {
+    label: 'SDA',
+    link: '',
+    badge: {
+      variant: 'info',
+      text: 'Student',
+    }
+  },
+
 ];
